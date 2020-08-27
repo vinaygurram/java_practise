f1a426656f4341c22a0c81b87d90223299c8603a
Vinay <g.vinayvenkatakrishna@gmail.com>
2020-08-27 11:18:51 +0530

    Added app and test files

---
+++ src/main/java/com/vinay/algos_java/App.java
@@ -0,0 +1,7 @@
+package com.vinay.algos_java;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Algos app");
+  }
+}
---
+++ src/test/java/com/vinay/algos_java/Test.java
@@ -0,0 +1,7 @@
+package com.vinay.algos_java;
+
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("This is testing");
+  }
+}
